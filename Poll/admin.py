170ad5343fa51fad23677ad04bd26a6eaa02c373
MJTech46 <140804026+MJTech46@users.noreply.github.com>
2024-05-27 13:19:17 +0530

    added Poll models (working)

--- Poll/admin.py
+++ Poll/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Poll, Option
 
 # Register your models here.
+
+admin.site.register(Option)
+admin.site.register(Poll)
--- Poll/models.py
+++ Poll/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class Poll(models.Model):
+    poll_text = models.CharField(max_length=255, null=False)
+    pub_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now=True)
+    is_pub = models.BooleanField(default=1)
+
+    def __str__(self):
+        return self.poll_text
+
+class Option(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='options')
+    option_text = models.CharField(max_length=255, null=False)
+    polled_users = models.ManyToManyField(User, blank=True)
+
+    def __str__(self):
+        return self.option_text
+    
